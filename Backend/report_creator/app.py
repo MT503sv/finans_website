82b5033898a29bc1adb1e60e63ab3e01a15ef583
Mateo PC <mateoantoniomenjivarmelara@gmail.com>
2026-06-09 19:20:49 -0600

    thrird try

--- Backend/report_creator/app.py
+++ Backend/report_creator/app.py
@@ -210,6 +210,6 @@ def download_excel():
         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
